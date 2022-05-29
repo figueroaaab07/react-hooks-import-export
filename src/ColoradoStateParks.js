d36049e9ec947924b4ad424793e8cc58c6bdf663
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-05-28 18:29:59 -0700

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,19 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  RMFunctions.wildlife();
+  console.log(RMFunctions.trees);
+  RMFunctions.elevation();
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
+
+export default ColoradoStateParks;
